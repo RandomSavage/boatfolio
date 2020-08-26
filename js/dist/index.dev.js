1dd939ebaf8708278179148360f3b5f2e10fd9d3
RandomSavage <50834695+RandomSavage@users.noreply.github.com>
2020-08-26 00:29:14 -0400

    building ot headerss and Marquee animations

--- js/dist/index.dev.js
+++ js/dist/index.dev.js
@@ -1,3 +1,23 @@
 "use strict";
 
-var _all = require("../node_modules/gsap/all.js");
+var _all = require("/../node_modules/gsap/all.js");
+
+var tl = _all.gsap.timeline();
+
+var msg = document.querySelector('.marquee-msg');
+var tween1;
+msg.addEventListener('mouseover', function () {
+  tween1 = _all.gsap.fromTo('.marquee-msg', {
+    x: 0,
+    y: 0,
+    color: '#fff'
+  }, {
+    duration: 2,
+    x: -245,
+    y: 0,
+    color: 'orangered',
+    yoyo: 'true',
+    repeat: -1,
+    opacity: 0
+  });
+});
--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-import {gsap} from '../node_modules/gsap/all.js';
+import {gsap} from '/../node_modules/gsap/all.js';
+
+let tl = gsap.timeline();
+
+let msg = document.querySelector('.marquee-msg');
+
+let tween1;
+
+msg.addEventListener('mouseover', () => {
+  tween1 = gsap.fromTo('.marquee-msg', {x: 0, y: 0, color: '#fff'}, {duration: 2, x: -245, y: 0, color: 'orangered', yoyo: 'true', repeat: -1, opacity: 0});
+})
