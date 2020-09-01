9faecb64e93dd1296c36788995cfde1b235f7781
RandomSavage <50834695+RandomSavage@users.noreply.github.com>
2020-09-01 10:51:56 -0400

    building php public file

---
+++ public/index.php
@@ -0,0 +1,195 @@
+<?php>
+  require('./includes/topScripts.php');
+<?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Boatfolio</title>
+  <link rel="stylesheet" href="sass/dist/_main.css">
+  <!-- This is a fontawesome alternative I am trying -->
+  <link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
+</head>
+<body>
+    <header>
+      <div class="left-side">
+        <div class="logo">
+        <a href="/">LOGO</a>
+        </div>
+        <nav class="top-nav">
+          <div class="nav-container">
+            <div><a href="tTops.php">T-Tops</a><span class="lnr lnr-chevron-down"></span>
+              <div class="dropdown-content">
+                <div class="content-container">
+                  <div class="image-container">
+                    <div class="img"></div>
+                  </div>
+                  <div class="info">
+                    <h4>Original Leaning Post</h4>
+                    <p>Includes three piece<br> Fishing Rod Holder<br>Stainless Steel</p>
+                    <div class ="button">SHOP NOW</div>
+                  </div>
+                  <div class="image-container">
+                    <div class="img"></div>
+                  </div>
+                  <div class="info">
+                    <h4>Original Leaning Post</h4>
+                    <p>Includes three piece<br> Fishing Rod Holder<br>Stainless Steel</p>
+                    <div class="button">SHOP NOW</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div><a href="#">Leaning Posts</a><span class="lnr lnr-chevron-down"></span>
+              <div class="dropdown-content2">
+                <div class="content-container">
+                  <div class="image-container">
+                    <div class="img"></div>
+                  </div>
+                  <div class="info">
+                    <h4>Original Leaning Post</h4>
+                    <p>Includes three piece<br> Fishing Rod Holder<br>Stainless Steel</p>
+                    <div class ="button">SHOP NOW</div>
+                  </div>
+                  <div class="image-container">
+                    <div class="img"></div>
+                  </div>
+                  <div class="info">
+                    <h4>Original Leaning Post</h4>
+                    <p>Includes three piece<br> Fishing Rod Holder<br>Stainless Steel</p>
+                    <div class ="button">SHOP NOW</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div id="third"><a href="#">Accessories</a><span class="lnr lnr-chevron-down"></span>
+              <div class="dropdown-content3">
+                <div class="content-container">
+                  <div class="image-container">
+                    <div class="img"></div>
+                  </div>
+                  <div class="info">
+                    <h4>Original Leaning Post</h4>
+                    <p>Includes three piece<br> Fishing Rod Holder<br>Stainless Steel</p>
+                    <div class ="button">SHOP NOW</div>
+                  </div>
+                  <div class="image-container">
+                    <div class="img"></div>
+                  </div>
+                  <div class="info">
+                    <h4>Original Leaning Post</h4>
+                    <p>Includes three piece<br> Fishing Rod Holder<br>Stainless Steel</p>
+                    <div class ="button">SHOP NOW</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div><a href="#">Customer Gallery</a></div>
+          </div>
+        </nav>
+        <div class="search">
+          <input type="search" name="search" class="top-search">
+          <span class="lnr lnr-magnifier"></span>
+        </div>
+      </div>
+      <div class="right-side">
+          <nav class="store-icons">
+            <ul>
+              <li><a href="#"><span class="lnr lnr-flag"></span></a></li>
+              <li><a href="#"><span class="lnr lnr-question-circle"></span></a></li>
+              <li><a href="#"><span class="lnr lnr-user"></span></a></li>
+              <li><a href="#"><span class="lnr lnr-cart"></span></a></li>
+            </ul>
+          </nav>
+      </div>
+    </header>
+    <div class="seasonal-notes"><h3>Note: Keep On Coding... Old Kid</h3></div>
+    <div class="marquee">
+      <div class="marquee-left"></div>
+      <div class="marquee-middle">
+        <span class="lnr lnr-chevron-left"></span>
+        <div class= marquee-msg>Free Shipping!!!</div>
+        <span class="lnr lnr-chevron-right"></span>
+      </div>
+      <div class="marquee-right"></div>
+    </div>
+    <div class="box-container">
+      <div class="box">SLIDE-01</div>
+      <div class="box">SLIDE-02</div>
+      <div class="box">SLIDE-03</div>
+      <div class="box">SLIDE-04</div>
+   </div>
+   <div class="footer-v1">
+    <div class="footer-v1-content">
+        <div class="col-sm-3 col-xs-12 nopaddingleft">
+            <a href="/" target="_self"><img class="lazy" src="https://fishmaster.com/pub/media/logo/Fishmaster-Logo-Footer.png" data-original="https://fishmaster.com/pub/media/logo/Fishmaster-Logo-Footer.png" alt="Fishmaster Logo" width="250" style="display: inline; max-width: 100%;"></a>
+            <p><i class="icon-line-mail" style="margin-right: 7px;"></i><a href="mailto:info@fishmaster.com">info@fishmaster.com</a>
+                <br>
+                <i class="icon  icon-phone3" style="margin-right: 7px;"></i><a href="tel:8777778693">877-777-8693</a>
+                <br>
+                <i class="icon-map-marker" style="margin-right: 7px;"></i><a href="https://goo.gl/maps/KxTEmnpunjaEhezW6" target="_blank">Knoxville, TN 37931 USA</a></p>
+        </div>
+
+        <div class="col-sm-2 col-xs-12 nopaddingleft">
+            <p class="footer-title">Shop By Category</p>
+            <ul class="footer links">
+                <li class="nav item"><a href="/t-tops.html">T-Tops</a></li>
+                <li class="nav item"><a href="/leaning-posts.html">Leaning Posts</a></li>
+                <li class="nav item"><a href="/t-top-accessories.html">T-Top Accessories</a></li>
+                <li class="nav item"><a href="/sales/guest/form/">Orders &amp; Returns</a></li>
+            </ul>
+        </div>
+        <div class="col-sm-2 col-xs-12 nopaddingleft">
+            <p class="footer-title">Company</p>
+            <ul class="footer links">
+                <li class="nav item"><a href="/about-fishmaster">About Us</a></li>
+                <li class="nav item"><a href="/blog">Blog</a></li>
+                <li class="nav item"><a href="/contact">Contact Us</a></li>
+                <li class="nav item"><a target="_blank" href="/pub/media/fishmaster/pdf/FMWarranty.pdf">Product Warranty</a></li>
+            </ul>
+        </div>
+        <div class="col-sm-2 col-xs-12 nopaddingleft">
+            <p class="footer-title">Support</p>
+            <ul class="footer links">
+                <li class="nav item"><a href="/support">Installation Manuals</a></li>
+                <li class="nav item"><a href="/knowledge-base">Help Center</a></li>
+                <li class="nav item"><a href="/fishmaster-dealers">Local Support</a></li>
+                <li class="nav item"><a href="/where-to-fish">Where To Fish</a></li>
+
+            </ul>
+        </div>
+        <div class="col-sm-3 col-xs-12 nopaddingleft" style="margin-top: 60px;">
+            <a title="NMMA" href="http://www.nmma.org/membership/join/accessory-manufacturers-division" target="_blank"><img class="lazy" src="/pub/media/home/nmma-member.svg" data-original="/pub/media/home/nmma-member.svg" style="max-width: 100%; display: inline;" alt="Proud member of NMMA"></a>
+        </div>
+        <div class="col-xs-12 border-v1">
+            <div class="pull-left-md">
+                <div class="newsletter-subscribe">
+                    <form action="https://fishmaster.com/newsletter/subscriber/new/" method="post" id="newsletter-footer" novalidate="novalidate">
+
+                        <div class="form-group">
+                            <input name="email" type="email" id="newsletter-bottom" placeholder="Enter your email address" data-validate="{required:true, 'validate-email':true}" class="input-text required-entry validate-email" style="width:auto; min-width:300px; margin: 10px 5px;">
+                            <button class="button" title="Subscribe" type="submit">
+                                <span>Sign Up</span>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="pull-right-md social-icons-v1">
+                <a class="social-icons si-borderless si-facebook" href="https://www.facebook.com/FishmasterInc/"> <i class="icon-facebook"></i> <i class="icon-facebook"></i> </a>
+                <a class="social-icons si-borderless si-youtube" href="https://www.youtube.com/channel/UCeI0g9TJuwtGah0p1ybk_DA"> <i class="icon-youtube"></i> <i class="icon-youtube"></i> </a>
+                <a class="social-icons si-borderless si-instagram" href="https://www.instagram.com/fishmasterinc/"> <i class="icon-instagram"></i> <i class="icon-instagram"></i> </a>
+            </div>
+        </div>
+        <div class="col-xs-12 footer-nav center">
+            <a href="#">Advanced Search</a> |
+            <a href="#">Privacy Policy</a> |
+            <a href="#">Terms &amp; Conditions</a>
+        </div>
+    </div>
+</div>
+  <script type="module" src="js/index.js"></script>
+</body>
+</html>
